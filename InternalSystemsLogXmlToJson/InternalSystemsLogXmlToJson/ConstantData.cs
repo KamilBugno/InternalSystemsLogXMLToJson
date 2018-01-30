1eeb4b8f33078404174621fb6a5b7e21bae5ea47
KamilBugno <bugno@student.agh.edu.pl>
2018-01-30 15:58:01 +0100

    add xml converter

---
+++ InternalSystemsLogXmlToJson/InternalSystemsLogXmlToJson/ConstantData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalSystemsLogXmlToJson
+{
+    static class ConstantData
+    {
+        public static string pathToXMLFile = @"D:\GeneratorLogówSystemówWewnętrznych\InternalSystemsLogXMLGenerator\Generator\Generator\bin\Debug\10-xml.xml";
+        public static string pathToJsonFile = @"D:\GeneratorLogówSystemówWewnętrznych\InternalSystemsLogXMLGenerator\Generator\Generator\bin\Debug\10-json.json";
+    }
+}
--- InternalSystemsLogXmlToJson/InternalSystemsLogXmlToJson/Program.cs
+++ InternalSystemsLogXmlToJson/InternalSystemsLogXmlToJson/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace InternalSystemsLogXmlToJson
 {
@@ -10,7 +14,20 @@ namespace InternalSystemsLogXmlToJson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string contents = File.ReadAllText(ConstantData.pathToXMLFile);
+
+            var a = Regex.Split(contents, "!#!");
+            a = a.Take(a.Count() - 1).ToArray();
+            foreach (string xml in a)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                var json = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.None, true);
+                Console.WriteLine(json);
+            }
+          
+            
+            
             Console.ReadKey();
         }
     }
