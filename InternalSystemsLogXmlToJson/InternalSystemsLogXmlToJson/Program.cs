db3c0a0695720623f4683f772210d84e013b35a7
KamilBugno <bugno@student.agh.edu.pl>
2018-01-30 15:15:47 +0100

    add initial project

---
+++ InternalSystemsLogXmlToJson/InternalSystemsLogXmlToJson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalSystemsLogXmlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
